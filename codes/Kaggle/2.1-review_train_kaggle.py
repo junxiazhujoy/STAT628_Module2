0fc0d27485c6a12f77efe6970625a83d282edb7d
junxiazhujoy <36055926+junxiazhujoy@users.noreply.github.com>
2019-03-26 15:29:28 -0500

    Update 2.1-review_train_kaggle.py

--- codes/Kaggle/2.1-review_train_kaggle.py
+++ codes/Kaggle/2.1-review_train_kaggle.py
@@ -4,10 +4,12 @@ import numpy as np
 import re
 import copy
 import nltk
+import stemming
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from nltk.stem import PorterStemmer
+from stemming.porter2 import stem
 
 
 data = [] 
@@ -15,7 +17,31 @@ for line in open('review_train.json', 'r'):
     data.append(json.loads(line))
 
 
-######!!!!!!!!!!!######                                                                                                  
+def generate_ngrams(review, n):
+    """
+    Generate n-grams from a review
+    :param review: one single review
+    :param n: the size of n gram
+    :return: generated list of n-grams
+    """
+    sentences = review.split(".")
+    n_grams = []
+    for sentence in sentences:
+        sections = sentence.split(",")
+        for section in sections:
+            parts = section.split(";")
+            for part in parts:
+                words = part.split()
+                
+                n_grams_temp = []    # the return list
+                for i in range(len(words)):
+                    temp = words[i]
+                    for j in range(1, n):
+                        if (i + j) < len(words):
+                            temp = temp + ' ' + words[i + j]
+                            n_grams.append(temp)
+    return n_grams
+
 dat = data[0:1000000]
 
 
@@ -35,10 +61,13 @@ def preprocess_reviews(reviews):
 
 reviewsTEXT_clean = copy.deepcopy(reviewslist)
 
+
+
 for ind in range(len(reviewslist)):
     texts = ''
     texts = reviewslist[ind]['text']
     texts = texts.lower()
+    texts = re.sub('n\'t',' not', texts)
     texts = re.sub('isnt','isn\'t', texts)
     texts = re.sub('wasnt','wasn\'t', texts)
     texts = re.sub('werent','weren\'t', texts)
@@ -81,14 +110,14 @@ for ind in range(len(reviewslist)):
         matchObj2 = re.search(r'(.*) never (.*)',list_text[i])
         
         if matchObj1 != None :
-            sub=re.sub(r' ', " notadd", matchObj1.group(2))
+            sub=re.sub(r' ', " notadd", ' '+matchObj1.group(2))
             list_text[i] = re.sub(matchObj1.group(2) ,sub, list_text[i])
             list_text[i] = re.sub(r'not ','', list_text[i],1)
             #list_text[i] = re.sub(',',' ', matchObj1.group(3))
             sent = sent + list_text[i] + ' '
         
         elif matchObj2 != None :
-            sub=re.sub(r' ', " notadd", matchObj2.group(2))
+            sub=re.sub(r' ', " notadd", ' '+matchObj2.group(2))
             list_text[i] = re.sub(matchObj2.group(2) ,sub, list_text[i])
             list_text[i] = re.sub(r'never ','', list_text[i],1)
             #list_text[i] = re.sub(',',' ', matchObj1.group(3))
@@ -102,20 +131,21 @@ for ind in range(len(reviewslist)):
     #print(texts)
     text_clean=preprocess_reviews(sent)
     #print(text_clean)
+    st = text_clean
+    st2 = " ".join([stem(word) for word in st.split(" ")])
+    text_clean = st2
+    
     reviewsTEXT_clean[ind]['text']=text_clean
     reviewsTEXT_clean[ind]['text_split']=text_clean.split()
-    ps = PorterStemmer()
-    stem=[]
-    for w in reviewsTEXT_clean[ind]['text_split']:
-        stem.append(ps.stem(w))
-    reviewsTEXT_clean[ind]['text_stem']=stem
-    reviewsTEXT_clean[ind]['freq']=nltk.FreqDist(reviewsTEXT_clean[ind]['text_stem'])
+    reviewsTEXT_clean[ind]['freq']=nltk.FreqDist(reviewsTEXT_clean[ind]['text_split'])   
+    reviewsTEXT_clean[ind]['bigrams']=generate_ngrams(text_clean, 2)
+    reviewsTEXT_clean[ind]['bigrams_freq']=nltk.FreqDist(reviewsTEXT_clean[ind]['bigrams'])
     
 #reviewsTEXT_clean[0]
 
 
-with open("review_train_pre.json", 'w') as fp:
-        json.dump(reviewsTEXT_clean, fp)
+#with open("review_train_pre.json", 'w') as fp:
+#        json.dump(reviewsTEXT_clean, fp)
 
 
 #reviewsTEXT_clean=[]
@@ -135,9 +165,7 @@ wordList = sorted(AllFreq.items(),key=lambda item:item[1],reverse=True)
 
 #choose the important ones--No.1-100
 WORDS=[x[0] for x in wordList]
-WORDS=WORDS[0:2000]
-
-
+WORDS=WORDS[0:1000]
 
 
 #stop_words = set(stopwords.words('english')) 
@@ -147,14 +175,16 @@ stop_words_stem=[]
 for w in stop_words:
     stop_words_stem.append(ps.stem(w))
 negWords=set(["wouldn't",'isn','wasn',"weren't", "haven't", "hasn't", "couldn't", "isn't", 'doesn','hasn',"mustn't", 'mightn', 'shan', 'no', "wasn't",'aren', "didn't", "hadn't","don't",'nor',"won't",'weren',"doesn't","needn't", 'shouldn',"mightn't","shan't", 'wouldn',"shouldn't",'hadn'])
-stop_words_negWords=[]
+negWords_stem=[]
 for w in negWords:
-    stop_words_negWords.append(ps.stem(w))
-d=set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','à'])
-stopWords=set(stop_words_stem)-set(stop_words_negWords)
-stopWords.update(d)
+    negWords_stem.append(ps.stem(w))
+d=set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','à','{sigh}'])
+stopWords_stem=set(stop_words_stem)-set(negWords_stem)
+stopWords_stem.update(d)
+stopWords_stem.add('was')
 
-WORDS=[x for x in WORDS if x not in stopWords]
+WORDS=[x for x in WORDS if x not in stopWords_stem]
+
 
 
 
@@ -208,6 +238,55 @@ print(Reviews_df.head(10))
 
 
 
+####add ngrams
+#select bigrams due to frequency order
+AllBiFreq = reviewsTEXT_clean[0]['bigrams_freq']
+for ind in range(len(reviewsTEXT_clean)-1):
+    Bifreqs = reviewsTEXT_clean[ind+1]['bigrams_freq']
+    AllBiFreq.update(Bifreqs)
+    
+BiwordList = sorted(AllBiFreq.items(),key=lambda item:item[1],reverse=True)
+BiwordList=BiwordList[0:500]
+
+#choose the important ones--No.1-100
+BiWORDS=[x[0] for x in BiwordList]
+Biwordfreq = {}
+for i in range(len(BiwordList)):
+    if len(list(set(BiwordList[i][0].split()).difference(stopWords_stem))) == 2:
+        Biwordfreq[BiwordList[i][0]]=BiwordList[i][1]
+
+BiWORDS=list(Biwordfreq.keys())
+
+for i in range(len(reviewsTEXT_clean)):
+    reviewsTEXT_clean[i]['text'] = ' '
+    for ind in reviewsTEXT_clean[i]['bigrams']:
+        if ind in BiWORDS:
+            reviewsTEXT_clean[i]['text'] = reviewsTEXT_clean[i]['text'] + ind +' '
+        else:
+            reviewsTEXT_clean[i]['text'] = reviewsTEXT_clean[i]['text']
+
+
+
+#create list of texts
+texts= [x['text'] for x in reviewsTEXT_clean]
+
+
+#vectorizer = CountVectorizer(token_pattern=r'\b[^\d\W]+\b')
+vectorizer = CountVectorizer(ngram_range=(2, 2))
+#X = vectorizer.fit_transform(texts)
+transformer = TfidfTransformer()
+
+tfidf = transformer.fit_transform(vectorizer.fit_transform(texts))
+
+X_ngrams=tfidf
+X_ngrams_df=pd.DataFrame(X_ngrams.A,columns = vectorizer.get_feature_names())
+print(X_ngrams_df.head(10))
+Reviews_ngrams_df=X_ngrams_df[list(set(Biwordfreq.keys()).intersection(set(vectorizer.get_feature_names())))]
+Reviews_ngrams_df
+print(Reviews_ngrams_df.head(10))
+
+Reviews_df=pd.concat([Reviews_df,Reviews_ngrams_df],axis=1)
+
 
 #Add Business ID in the data frame
 businessIDs=[x['business_id'] for x in reviewsTEXT_clean]
@@ -233,14 +312,3 @@ DataFrame.to_csv(Reviews_df,"review_train_kaggle.csv",index=False)
 #col=Reviews_df.columns.values
 
 #DataFrame.to_csv(pd.DataFrame(col),"review_train_kaggle_colnames.csv",index=False)
-
-
-
-
-
-
-
-
-
-
-
